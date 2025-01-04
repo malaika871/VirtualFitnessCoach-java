5836139cd4d499f1a907a9605e6f3050c5cd4b0b
malaika871 <123286921+malaika871@users.noreply.github.com>
2025-01-04 23:30:16 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,141 +1,110 @@
-import java.util.List;
 import java.util.Scanner;
 
-interface NutritionStrategy{
-    void display();
+interface WorkoutPlan {
+    void workout();
 }
 
-class NutritionPlan{
-    private int id;
-    private List<String> meals;
-    private String dietType;
-    private int Calories;
+class WeightLossPlan implements WorkoutPlan{
 
-    // Constructors
-    public NutritionPlan(int id, String dietType, int Calories){
-        this.id = id;
-        this.dietType = dietType;
-        this.Calories = Calories;
-    }
-
-    // setter & getter
-    public int getId(){
-        return id;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public List<String> getMeals(){
-        return meals;
-    }
-
-    public String getDietType(){
-        return dietType;
-    }
-
-    public void setDietType(String dietType){
-        this.dietType = dietType;
-    }
-
-    public int getCalories(){
-        return Calories;
-    }
-
-    public void setCalories(int Calories) {
-        this.Calories = Calories;
-    }
-
-}
-class KetoDietPlan implements NutritionStrategy{
-    public void display(){
-        System.out.println("Keto Diet Plan. ");
-        System.out.println("1. Breakfast: Toast with Scrambled eggs and bacons");
-        System.out.println("2. Lunch: Chicken burger with jalapeno");
-        System.out.println("3. Dinner: Meatballs with Cheese");
-        System.out.println("3. Snacks: Cheese Sticks, Boiled Eggs, Mixed Nuts");
+    public void workout(){
+        System.out.println("Weight loss Workout Plan: ");
+        System.out.println("1. Warm Up: 10 min walk. ");
+        System.out.println("2. 30 seconds sprints, 1 minute rest (repeat 10 times). ");
+        System.out.println("3. Strength: Squats adn push ups (3 sets of 15 reps each). ");
+        System.out.println("4. Cool Down: Stretch for 5 minutes. ");
 
     }
 }
 
-class VegetarianDietPlan implements NutritionStrategy{
-    public void display(){
-        System.out.println("Vegetarian Diet Plan. ");
-        System.out.println("1. Breakfast: Toast with avocado and tomatoes.");
-        System.out.println("2. Lunch: Stir-Fry Tofu wrap with lettuce");
-        System.out.println("3. Dinner: Lentils soup with Beans");
-        System.out.println("3. Snacks: Fruit Salad, Chickpeas");
+class MuscleGainPlan implements WorkoutPlan{
+    public void workout(){
+        System.out.println("Muscle Gain Workout Plan. ");
+        System.out.println("1. Warm up: 5-10 minutes treadmill. ");
+        System.out.println("2. Chest:Bench Press (4 sets of 8-10 reps). ");
+        System.out.println("3. Back: DeadLifts (4 sets of 6-8 reps). ");
+        System.out.println("4. Legs: Squats (4 sets of 8-10 reps). ");
+        System.out.println("5. Biceps: Barbell Curls (3 sets of 6-8 reps). ");
+        System.out.println("6. Triceps: Tricep Dips (3 sets of 6-8 reps). ");
+        System.out.println("7. Cool Down: Stretching. ");
+    }
+}
+
+class CardioPlan implements WorkoutPlan{
+    public void workout(){
+        System.out.println("Cardio Workout Plan. ");
+        System.out.println("1. Warm up: 5 minutes jogging or walking. ");
+        System.out.println("2. Running: 30 minutes. ");
+        System.out.println("3. Cool Down: Stretching for 5 minutes. ");
+    }
+}
+
+class StrengthPlan implements WorkoutPlan{
+    public void workout(){
+        System.out.println("Strengthening Plan. ");
+        System.out.println("4. Warm up: Push Ups 10 minutes. ");
+        System.out.println("4. Weight Lifting: 3 sets of 10 reps. ");
+        System.out.println("4. Planks: 1 minute hold. ");
 
     }
 }
 
-class BalancedDietPlan implements NutritionStrategy{
-    public void display(){
-        System.out.println("Balanced Diet Plan. ");
-        System.out.println("1. Breakfast: Oatmeal with fruits and juices");
-        System.out.println("2. Lunch: Quinoa salad with mixed veggies");
-        System.out.println("3. Dinner: Chicken with brown rice and veggies");
-        System.out.println("3. Snacks: Greek Yogurt, Fresh Fruits");
-
-    }
-}
-
-class WeightLossDietPlan implements NutritionStrategy{
-    public void display(){
-        System.out.println("Weight Loss Diet Plan. ");
-        System.out.println("1. Breakfast: Green smoothie with spinach and chia seeds.");
-        System.out.println("2. Lunch: Grilled Chicken salad with Olive oil dressing.");
-        System.out.println("3. Dinner: Baked fish with steamed vegetables.");
-        System.out.println("3. Snacks: Rice Cakes, Peanut Butter, low-calorie bars");
+class Yoga implements WorkoutPlan{
+    public void workout(){
+        System.out.println("Yoga Workout Plan. ");
+        System.out.println("1. Breathing Exercise: 5 minutes. ");
+        System.out.println("2. Forward Bending: 15 rounds. ");
+        System.out.println("4. Sun Salutations: 10 rounds. ");
+        System.out.println("3. Meditation: 10 minutes. ");
 
     }
 }
 
 // context class
-class NutritionContext{
-    NutritionStrategy nutritionStrategy;
+class WorkoutContext{
+    WorkoutPlan WorkOutPlan;
 
-    public void setNutritionStrategy(NutritionStrategy nutritionStrategy){
-        this.nutritionStrategy = nutritionStrategy;
+    public  void setWorkoutPlan(WorkoutPlan WorkPlan){
+        this.WorkOutPlan = WorkPlan;
     }
 
-    public void displayPlan(){
-        nutritionStrategy.display();
+    public void performWorkout(){
+        WorkOutPlan.workout();
     }
-
 }
 
+// main class
 public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        NutritionContext Context = new NutritionContext();
+        Scanner scanner= new Scanner(System.in);
+        WorkoutContext Context = new WorkoutContext();
 
-        System.out.println("Choose a Nutrition Plan.");
-        System.out.println("1. Keto Diet Plan (non-veg).");
-        System.out.println("2. Vegetarian Diet Plan.");
-        System.out.println("3. Balanced Diet Plan.");
-        System.out.println("4. Weight Loss Diet Plan.");
-
+        System.out.println("Choose your Work Out Plan. ");
+        System.out.println("1. Weight Loss Workout Plan. ");
+        System.out.println("2. Muscle Gain Workout Plan. ");
+        System.out.println("3. Cardio Workout Plan. ");
+        System.out.println("4. Strengthening Workout Plan. ");
+        System.out.println("5. Yoga Workout Plan. ");
+        System.out.println("Enter your choice (in number)");
+        System.out.println();
         int choice = scanner.nextInt();
-        switch(choice){
-            case 1:
-            Context.setNutritionStrategy(new KetoDietPlan());
-            break;
-            case 2:
-            Context.setNutritionStrategy(new VegetarianDietPlan());
-            break;
-            case 3:
-            Context.setNutritionStrategy(new BalancedDietPlan());
-            break;
-            case 4:
-            Context.setNutritionStrategy(new WeightLossDietPlan());
-            break;
-            default:
-            System.out.println("Invalid Input.");
-            break;
+        switch(choice){ 
+        case 1:
+        Context.setWorkoutPlan(new WeightLossPlan());
+        break;
+        case 2:
+        Context.setWorkoutPlan(new MuscleGainPlan());
+        break;
+        case 3: 
+        Context.setWorkoutPlan(new CardioPlan());
+        break;
+        case 4:
+        Context.setWorkoutPlan(new StrengthPlan());
+        break;
+        case 5:
+        Context.setWorkoutPlan(new Yoga());
+        break;
         }
-        Context.displayPlan();
+        Context.performWorkout();
         scanner.close();
     }
 }
